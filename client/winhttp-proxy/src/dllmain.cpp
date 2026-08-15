1861cc1f2ccfa478e65e62d6d40d45707b37b698
Burke <burkeseeger@gmail.com>
2026-08-15 01:47:02 -0400

    Source code for winhttp.dll

---
+++ client/winhttp-proxy/src/dllmain.cpp
@@ -0,0 +1,29 @@
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+#include "forwarders.g.h"
+#include "connect_hook.h"
+
+static DWORD WINAPI InitThread(LPVOID) {
+    // This whole thing is just to forward traffic to 127.0.0.1:2050 
+    // so that we can intercept the packets before sending them to the server.
+    InstallConnectHook();
+    return 0;
+}
+
+BOOL WINAPI DllMain(HINSTANCE hinst, DWORD reason, LPVOID /*reserved*/) {
+    switch (reason) {
+        case DLL_PROCESS_ATTACH: {
+            DisableThreadLibraryCalls(hinst);
+            HANDLE t = CreateThread(nullptr, 0, InitThread, nullptr, 0, nullptr);
+            if (t) CloseHandle(t);
+            break;
+        }
+        case DLL_PROCESS_DETACH:
+            RemoveConnectHook();
+            break;
+        default:
+            break;
+    }
+    return TRUE;
+}
